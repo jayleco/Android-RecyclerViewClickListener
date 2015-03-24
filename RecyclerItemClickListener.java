b3eba2b081c4853712c0a2564ca3ece921e077e3
JT <pandawarrior91@gmail.com>
2015-03-25 00:03:22 +0800

    experimenting with more touch features, such as scroll and fling.

--- RecyclerItemClickListener.java
+++ RecyclerItemClickListener.java
@@ -1,7 +1,9 @@
-package org.pandacat.jt.pandacatbudget.Utilities;
+package com.example.liew.myapplication.clicking; //change your package
+
+import android.support.v7.widget.RecyclerView;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
@@ -13,36 +15,70 @@ public class RecyclerItemClickListener implements RecyclerView.OnItemTouchListen
     public static interface OnItemClickListener{
         public void onItemClick(View view, int position);
         public void onItemLongClick(View view, int position);
+        public void onFling(View view, int position);
     }
 
 
     private OnItemClickListener mListener;
     private GestureDetector mGestureListener;
 
-    public RecyclerItemClickListener(Context context, final RecyclerView recyclerView, OnItemClickListener listener){
+    public RecyclerItemClickListener(final Context context, final RecyclerView recyclerView, OnItemClickListener listener){
         mListener = listener;
-        mGestureListener = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener(){
-            public boolean onSingleTapUp(MotionEvent e){
-                return true;
+
+        mGestureListener = new GestureDetector(context, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                Log.d("Gesture", "onDown");
+                return false;
             }
 
-            public void onLongPress(MotionEvent e){
+            @Override
+            public void onShowPress(MotionEvent e) {
+                Log.d("Gesture", "onShowPress");
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                Log.d("Gesture", "onSingleTapUp");
+                View childView = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                if(childView != null && mListener != null){
+                    mListener.onItemClick(childView, recyclerView.getChildAdapterPosition(childView));
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                Log.d("Gesture", "onScroll");
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+                Log.d("Gesture", "onLongPress");
                 View childView = recyclerView.findChildViewUnder(e.getX(), e.getY());
                 if(childView != null && mListener != null){
                     mListener.onItemLongClick(childView, recyclerView.getChildAdapterPosition(childView));
                 }
             }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                Log.d("Gesture", "onFling");
+                View childView = recyclerView.findChildViewUnder(e1.getX(), e1.getY());
+                if(childView != null && mListener != null){
+                    mListener.onFling(childView, recyclerView.getChildAdapterPosition(childView));
+                }
+                return false;
+            }
         });
     }
 
     public boolean onInterceptTouchEvent(RecyclerView view, MotionEvent e){
-        View childView = view.findChildViewUnder(e.getX(), e.getY());
-        if(childView != null && mListener != null && mGestureListener.onTouchEvent(e)){
-            mListener.onItemClick(childView, view.getChildAdapterPosition(childView));
-        }
+        mGestureListener.onTouchEvent(e);
         return false;
     }
-    //TODO put onTouchEvent later 3
+
     public void onTouchEvent(RecyclerView view, MotionEvent motionEvent){
 
     }
